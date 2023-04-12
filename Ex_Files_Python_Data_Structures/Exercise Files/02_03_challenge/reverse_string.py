9ef0196272fc9faf0a6a1f0b55c5a80a7a42a670
Eric Mangin <ericpaulmangin@gmail.com>
2023-04-12 13:25:02 -0700

    Completed the Stack Class and used its methods to reverse a string within Exercise Files >> 02_03_challenge folder.

--- Ex_Files_Python_Data_Structures/Exercise Files/02_03_challenge/reverse_string.py
+++ Ex_Files_Python_Data_Structures/Exercise Files/02_03_challenge/reverse_string.py
@@ -12,4 +12,17 @@ s = stack.Stack()
 
 # Your solution here.
 
-print(reversed_string)
+for char in string:
+    s.push(char)
+
+while not s.is_empty():
+    reversed_string += s.pop()
+
+print("  ═══════════════════════════════════════════════════  ")
+print("Let's use a Stack Data Structure to reverse the String: ")
+print("       ==>     '" + string + "': ")
+print("                                                       ")
+print("The answer is: " + "'" + reversed_string+"'.")
+print("                                                       ")
+print("              ⇢ ⇢ ⇢ Hooray for Python! ⇠ ⇠ ⇠")
+print("  ═══════════════════════════════════════════════════  ")
